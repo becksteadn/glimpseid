f884bdd6cdd0c161ba5a9da4f9bee79c8a852627
Nathaniel Beckstead <becksteadn@gmail.com>
2019-03-06 11:37:01 -0500

    we stylin

--- glimpse.js
+++ glimpse.js
@@ -3,6 +3,7 @@ var API_URL = "https://x73ay0u3sb.execute-api.us-east-1.amazonaws.com/Beta/"
 function screenshot(url) {
     $.when($.ajax(API_URL + "screenshot?url=" + encodeURIComponent(url))).done(function(data, textStatus, jqXHR) {
         var json = JSON.parse(jqXHR.responseText);
+        $("#screenshot-viewer").css("box-shadow", "10px 10px 10px 10px gray");
         $("#screenshot-viewer").attr("src", json.screenshot);
         $("#screenshot-viewer").height("700px");
         console.log(jqXHR.responseText);
