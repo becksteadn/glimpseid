ee6a19072a20ee9805f35034205760d692c2a68f
Nathaniel Beckstead <becksteadn@gmail.com>
2019-03-07 13:36:49 -0500

    ga tracking

--- glimpse.js
+++ glimpse.js
@@ -2,6 +2,7 @@ var API_URL = "https://x73ay0u3sb.execute-api.us-east-1.amazonaws.com/Beta/"
 
 function screenshot_url(submitForm) {
     //$(".scan-status").text("Scanning in progress.");
+    $(".url-display").text("")
     $("#screenshot-viewer").height("200px");
     $("#screenshot-viewer").css("box-shadow", "none")
     $("#screenshot-viewer").attr("src", "loading-bars.svg")
@@ -15,6 +16,12 @@ function screenshot_url(submitForm) {
 function screenshot(url) {
     $.when($.ajax(API_URL + "screenshot?url=" + encodeURIComponent(url))).done(function(data, textStatus, jqXHR) {
         var json = JSON.parse(jqXHR.responseText);
+        if (json.hasOwnProperty('errorMessage')){
+            $("#screenshot-viewer").attr("src", "");
+            $(".url-display").text("The scan failed.")
+            console.log("Scan failed.")
+            return false
+        }
         $("#screenshot-viewer").css("box-shadow", "10px 10px 10px 10px gray");
         $("#screenshot-viewer").attr("src", json.screenshot);
         $("#screenshot-viewer").height("700px");
