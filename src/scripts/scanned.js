9667f4656078c6928cf4f8727e2542b15798e6be
Nathaniel Beckstead <becksteadn@gmail.com>
2019-04-05 18:33:31 -0400

    not available error catching

--- src/scripts/scanned.js
+++ src/scripts/scanned.js
@@ -6,7 +6,7 @@ var scan_app = new Vue({
         effectiveurl: 'Effective URL',
         time: 'Time',
         numscans: 'Number of Scans',
-        screenshot_url: 'https://blahblahblah.blah',
+        screenshot_url: 'images/notavailable.png',
         img_height: '600px'
     }
 })
@@ -14,14 +14,18 @@ var scan_app = new Vue({
 function get_scan_data(){
     $.when($.ajax(API_URL + "scan/" + $.urlParam('hash')).done(function(data, textStatus, jqXHR) {
         var json = JSON.parse(jqXHR.responseText);
-        scan_app.title = json.Item.title.S;
-        scan_app.url = json.Item.url.S;
-        scan_app.effectiveurl = json.Item.effectiveurl.S;
-        scan_app.time = "Scanned at " + json.Item.timescanned.S;
-        scan_app.numscans = "Scanned " + json.Item.numscans.N + " times.";
-        scan_app.screenshot_url = "https://glimpsefiles.s3.amazonaws.com/" + ENV + "/screenshots/" + json.Item.urlhash.S + ".png";
-        scan_app.img_height = "600px";
-        $("#screenshot-viewer").height("600px");
+        if (json.Item != null){
+            scan_app.title = json.Item.title.S;
+            scan_app.url = json.Item.url.S;
+            scan_app.effectiveurl = json.Item.effectiveurl.S;
+            scan_app.time = "Scanned at " + json.Item.timescanned.S;
+            scan_app.numscans = "Scanned " + json.Item.numscans.N + " times.";
+                scan_app.screenshot_url = "https://glimpsefiles.s3.amazonaws.com/" + ENV + "/screenshots/" + json.Item.urlhash.S + ".png";
+            scan_app.img_height = "600px";
+            $("#screenshot-viewer").height("600px");
+        } else {
+            $("#screenshot-viewer").height("300px");
+        }
     }));
 }
 
