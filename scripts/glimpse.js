52402176304668d6cae165b87c4c9b959d2854c9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-06 00:46:11 +0000

    Deploy becksteadn/glimpseid to github.com/becksteadn/glimpseid.git:gh-pages

--- scripts/glimpse.js
+++ scripts/glimpse.js
@@ -6,7 +6,7 @@ function screenshot_url(submitForm) {
     $(".url-display").text("")
     $("#screenshot-viewer").height("200px");
     $("#screenshot-viewer").css("box-shadow", "none")
-    $("#screenshot-viewer").attr("src", "loading-bars.svg")
+    $("#screenshot-viewer").attr("src", "images/loading-bars.svg")
     //$(".url-display").text(submitForm.url.value);
     $.when(screenshot(submitForm.url.value)).done(function() {
        //$(".scan-status").text("Scan complete.");
@@ -39,17 +39,4 @@ $.urlParam = function(name){
        return null;
     }
     return decodeURI(results[1]) || 0;
-}
-
-function get_scan_data(){
-    $.when($.ajax(API_URL + "scan/" + $.urlParam('hash')).done(function(data, textStatus, jqXHR) {
-        var json = JSON.parse(jqXHR.responseText);
-        $("#title").text(json.Item.title.S);
-        $("#url").text(json.Item.url.S);
-        $("#effectiveurl").text(json.Item.effectiveurl.S);
-        $("#time").text("Scanned at " + json.Item.timescanned.S);
-        $("#numscans").text("Scanned " + json.Item.numscans.N + " times.");
-        $("#screenshot-viewer").attr("src", "https://glimpsefiles.s3.amazonaws.com/" + ENV + "/screenshots/" + json.Item.urlhash.S + ".png");
-        $("#screenshot-viewer").height("600px");
-    }));
 }
