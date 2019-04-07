bf0627504f999657ec84b522b698f8da0bedf77d
Nathaniel Beckstead <becksteadn@gmail.com>
2019-04-06 23:15:46 -0400

    correct spelling of import

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from selenium imprt webdriver
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver import Firefox
