dfdf68b75d3a88b636282805c6c8308daf539073
Nathaniel Beckstead <becksteadn@gmail.com>
2019-04-07 10:57:18 -0400

    improve testing

--- tests/test.py
+++ tests/test.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+#from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 class Tester:
     def __init__(self, base_url):
@@ -11,9 +12,9 @@ class Tester:
         #chrome_options.add_argument('--headless')
         #chrome_options.add_argument('--window-size=1920x1080')
         #self.driver = webdriver.Chrome(chrome_options=chrome_options)
-        firefox_options = Options()
-        firefox_options.add_argument('--headless')
-        self.driver = Firefox(firefox_options=firefox_options)
+        firefox_options = FirefoxOptions()
+        firefox_options.headless = True
+        self.driver = Firefox(options=firefox_options)
         self.base_url = base_url
         self.driver.get(self.base_url)
 
@@ -30,15 +31,19 @@ class Tester:
     def get_scan_result(self, url):
         self.driver.get('SOMETHING/scan/{ hash_of_url }')
 
+    def press_scan(self):
+        scan_btn = self.driver.find_element_by_id('btn-override')
+        scan_btn.click()
+
     def enter_url(self, url):
-        url_input = self.driver.find_element_by_class_name('form-control')[0]
+        #url_input = self.driver.find_elements_by_class_name('form-control')
+        #print(url_input)
+        #url_input = url_input[0]
+        url_input = self.driver.find_element_by_id('input-url')
         if url_input:
             url_input.send_keys(url)
         else:
             raise("Couldn't find URL form input by class name")
-
-        scan_btn = self.driver.find_element_by_id('btn-override')
-        scan_btn.click()
 
     def force_update(self):
         checkmark = self.driver.find_element_by_id('force-update')
