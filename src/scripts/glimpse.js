c55691d72cf7e26fd5a79fb33ce9d197fe4177e4
Nathaniel Beckstead <becksteadn@gmail.com>
2019-04-08 20:45:43 -0400

    custom user-agent ui

--- src/scripts/glimpse.js
+++ src/scripts/glimpse.js
@@ -4,9 +4,9 @@ var API_URL = "https://x73ay0u3sb.execute-api.us-east-1.amazonaws.com/" + ENV + 
 function screenshot_url(submitForm) {
     //$(".scan-status").text("Scanning in progress.");
     $(".url-display").text("")
-    $("#screenshot-viewer").height("200px");
-    $("#screenshot-viewer").css("box-shadow", "none")
-    $("#screenshot-viewer").attr("src", "images/loading-bars.svg")
+    $("#loading-gif").height("200px");
+    $("#loading-gif").css("box-shadow", "none")
+    $("#loading-gif").attr("src", "images/loading-bars.svg")
     //$(".url-display").text(submitForm.url.value);
     $.when(screenshot(submitForm.url.value)).done(function() {
        //$(".scan-status").text("Scan complete.");
@@ -19,16 +19,16 @@ function screenshot(url) {
     $.when($.ajax(API_URL + "scan?url=" + encodeURIComponent(url) + "&update=" + force_update).done(function(data, textStatus, jqXHR) {
         var json = JSON.parse(jqXHR.responseText);
         if (json.hasOwnProperty('errorMessage')){
-            $("#screenshot-viewer").attr("src", "");
+            $("#loading-gif").attr("src", "");
             $(".url-display").text("The scan failed.");
             console.log("Scan failed.");
             return false;
         }
         window.location = "scan.html?hash="  + json.urlhash;
         return false;
-        $("#screenshot-viewer").css("box-shadow", "10px 10px 10px 10px gray");
-        $("#screenshot-viewer").attr("src", json.screenshot);
-        $("#screenshot-viewer").height("700px");
+        $("#loading-gif").css("box-shadow", "10px 10px 10px 10px gray");
+        $("#loading-gif").attr("src", json.screenshot);
+        $("#loading-gif").height("700px");
         console.log(jqXHR.responseText);
     }));
 }
@@ -40,13 +40,3 @@ $.urlParam = function(name){
     }
     return decodeURI(results[1]) || 0;
 }
-
-
-window.addEventListener('load', function() {
-    scan_options = new Vue({
-        el: '#sect-options',
-        data: {
-            seen: false
-        }
-    })
-})
